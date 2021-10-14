d8e39630a59d5b8aed62e0bf3df533a95b34301e
saishamhala <saishamhala@gmail.com>
2021-10-14 15:41:35 +0530

    Update Leaderboard.java

--- src/main/java/com/sprintOne/model/Leaderboard.java
+++ src/main/java/com/sprintOne/model/Leaderboard.java
@@ -1,99 +1,105 @@
 package com.sprintOne.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "Leaderboard")
+
 public class Leaderboard {
 	
 	@Id
+	@Column(name = "bidder_id")
 	private int bidder_id;
-	private String bidder_name;
-	private int bidder_points;
-	private int bids_participated;
-	private int bids_won;
+	
+	@OneToOne
+	@JoinColumn(name = "name")
+	@Column(name = "bidder_name")
+	private Bidder biddername;
+	
+	@OneToOne
+	@JoinColumn(name = "points")
+	@Column(name = "bidder_points")
+	private Bidder bidderpoints;
+	
+	@Column(name = "bids_participated")
+	private String bidsparticipated;
+	
+	@Column(name = "bids_won")
+	private int bidswon;
+	
+	@Column(name = "percentile")
 	private float percentile;
 	
 	public Leaderboard() {
 		
 	}
-	
-	
-	public Leaderboard(int bidder_id, String bidder_name, int bidder_points, int bids_participated, 
-			int bids_won, float percentile) {
+
+	public Leaderboard(int bidder_id, Bidder biddername, Bidder bidderpoints, String bidsparticipated, int bidswon,
+			float percentile) {
 		super();
 		this.bidder_id = bidder_id;
-		this.bidder_name = bidder_name;
-		this.bidder_points = bidder_points;
-		this.bids_participated = bids_participated;
-		this.bids_won = bids_won;
+		this.biddername = biddername;
+		this.bidderpoints = bidderpoints;
+		this.bidsparticipated = bidsparticipated;
+		this.bidswon = bidswon;
 		this.percentile = percentile;
 	}
-
 
 	public int getBidder_id() {
 		return bidder_id;
 	}
 
-
 	public void setBidder_id(int bidder_id) {
 		this.bidder_id = bidder_id;
 	}
 
-
-	public String getBidder_name() {
-		return bidder_name;
+	public Bidder getBiddername() {
+		return biddername;
 	}
 
-
-	public void setBidder_name(String bidder_name) {
-		this.bidder_name = bidder_name;
+	public void setBiddername(Bidder biddername) {
+		this.biddername = biddername;
 	}
 
-
-	public int getBidder_points() {
-		return bidder_points;
+	public Bidder getBidderpoints() {
+		return bidderpoints;
 	}
 
-
-	public void setBidder_points(int bidder_points) {
-		this.bidder_points = bidder_points;
+	public void setBidderpoints(Bidder bidderpoints) {
+		this.bidderpoints = bidderpoints;
 	}
 
-
-	public int getBids_participated() {
-		return bids_participated;
+	public String getBidsparticipated() {
+		return bidsparticipated;
 	}
 
-
-	public void setBids_participated(int bids_participated) {
-		this.bids_participated = bids_participated;
+	public void setBidsparticipated(String bidsparticipated) {
+		this.bidsparticipated = bidsparticipated;
 	}
 
-
-	public int getBids_won() {
-		return bids_won;
+	public int getBidswon() {
+		return bidswon;
 	}
 
-
-	public void setBids_won(int bids_won) {
-		this.bids_won = bids_won;
+	public void setBidswon(int bidswon) {
+		this.bidswon = bidswon;
 	}
-
 
 	public float getPercentile() {
 		return percentile;
 	}
 
-
 	public void setPercentile(float percentile) {
 		this.percentile = percentile;
 	}
 	
+    
 	
 	
-	
-	
-	
-
 }
+	
