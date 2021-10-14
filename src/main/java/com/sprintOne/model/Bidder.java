6c31be2b956502434fe2386e041f9bbfa6e71f12
Chaturvedi-Animesh <52943258+Chaturvedi-Animesh@users.noreply.github.com>
2021-10-14 12:10:17 +0530

    Initial commit

---
+++ src/main/java/com/sprintOne/model/Bidder.java
@@ -0,0 +1,85 @@
+package com.sprintOne.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Bidder {
+	
+	@Id
+	private int userID;
+	private String name;
+	private String email;
+	private String password;
+	private String mobile;
+	private int points;
+	
+	public Bidder() {
+		
+	}
+	
+	public Bidder(int userID, String name, String email, String password, String mobile, int points) {
+		super();
+		this.userID = userID;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.mobile = mobile;
+		this.points = points;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/test/java/com/sprintOne/IPLFantasy/IplFantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintOne.IPLFantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplFantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
