dfc419ee328c63576a20d8356c0b050c5cc2b96b
Hiba-TP <hiba.suman20@gmail.com>
2021-10-16 17:50:35 +0530

    Update BidderController.java

--- src/main/java/com/sprintOne/controller/BidderController.java
+++ src/main/java/com/sprintOne/controller/BidderController.java
@@ -2,10 +2,10 @@ package com.sprintOne.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +19,6 @@ import com.sprintOne.model.Leaderboard;
 import com.sprintOne.model.MatchDetails;
 import com.sprintOne.model.TeamDetails;
 import com.sprintOne.model.TeamPointsTable;
-import com.sprintOne.service.AdminService;
 import com.sprintOne.service.BidderService;
 
 @RestController
