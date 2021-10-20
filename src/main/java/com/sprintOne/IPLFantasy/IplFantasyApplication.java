fdc9b4ae82947b51949ccf797ae00be9dbcc0b1b
Chaturvedi-Animesh <52943258+Chaturvedi-Animesh@users.noreply.github.com>
2021-10-20 08:33:26 +0530

    Update IplFantasyApplication.java

--- src/main/java/com/sprintOne/IPLFantasy/IplFantasyApplication.java
+++ src/main/java/com/sprintOne/IPLFantasy/IplFantasyApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication(scanBasePackages = {"com.sprintOne"})
 @EnableJpaRepositories("com.sprintOne.dao")
 @EntityScan("com.sprintOne.model")
+@EnableSwagger2
 public class IplFantasyApplication {
 
 	public static void main(String[] args) {
